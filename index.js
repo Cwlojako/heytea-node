b2abc928ed1a102f0cc4248dc9bee0912be91980
2023030106 <chenwei11@keshun.com.cn>
2025-04-11 18:08:25 +0800

    feature: 功能完善；陈尉

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ const Schema = mongoose.Schema
 
 mongoose.connect('mongodb://cwlojako:chenweiqq0@47.106.130.54:27017/heytea')
 
-// Define Token Schema and Model
 const tokenSchema = new Schema({
 	value: { type: String, required: true },
 	createdAt: { type: Date, default: Date.now }
 })
 const Token = mongoose.model('Token', tokenSchema)
+
+const orderSignalSchema = new Schema({
+    order_no: { type: String, required: true },
+    signal: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+})
+const OrderSignal = mongoose.model('OrderSignal', orderSignalSchema)
 
 const corsOptions = {
 	// origin: 'http://47.106.130.54:8000'
@@ -32,7 +38,6 @@ app.get('/setToken', async (req, res) => {
 		return
 	}
 	try {
-		// Save token to MongoDB
 		const newToken = new Token({ value: token })
 		await newToken.save()
 		res.send({ code: 200, message: '设置成功', token: newToken })
@@ -91,243 +96,195 @@ app.post('/findGoods', async (req, res) => {
 	}
 })
 
-// 保存订单
-// app.post('/saveOrder', async (req, res) => {
-// 	try {
-// 		const { uuid, buyer, list, price, deliverCount, profit } = req.body
-// 		const order = new Order({ uuid, buyer, list, price, deliverCount, profit })
-// 		await order.save()
-// 		res.send({ code: 200, message: '保存成功', order })
-// 	} catch(error) {
-// 		res.status(400).send(error)
-// 	}
+// 商品详情
+app.post('/goodsDetail', async (req, res) => {
+	const { productId, url, shopId } = req.body
+	try {
+		const { data: static_info } = await axios.get(`https://static-cos-menu.heytea.com${url}`)
+		const { data: dynamic_info } = await axios.post(`https://go.heytea.com/api/service-menu/grayapi/dynamic/menu/product-info?shopId=${shopId}`, {
+			productIds: [productId],
+			shopId
+		}, {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		static_info.skus.forEach(e => {
+			e.material_groups.forEach(f => {
+				f.materials.forEach(g => {
+					g.is_enable = dynamic_info.data.products[0].skus[e.id].material[g.id].is_enable
+				})
+			})
+		})
+		res.send({ code: 200, message: '获取成功', data: static_info })
+	} catch (err) {
+		res.send({ code: err.status, message: err.data?.message || '请求失败' })
+	}
+})
 
-// })
+// 购买下单
+app.post('/settle', async (req, res) => {
+	let { products, shopId, price, signal, couponId } = req.body
+	try {
+		const latestToken = await Token.findOne().sort({ createdAt: -1 }); // 按创建时间降序排序，取最新的 token
+		if (!latestToken) {
+			return res.status(400).send({ code: 400, message: '未找到有效的 token，请先设置 token' });
+		}
+		if (couponId) {
+			const { data: coupon } = await axios.post(`https://vip.heytea.com/api/service-coupon/couponLibrary/detail?id=${couponId}`, {}, {
+				headers: {
+					'Authorization': latestToken.value
+				}
+			})
+			if (coupon.data.couponType === 0) {
+				const p = price - +coupon.data.discountText
+				price = p < 0 ? 0 : p
+			} else if (coupon.data.couponType === 3) {
+				price = (price * (+coupon.data.discountText / 10)).toFixed(2)
+			}
+		}
+		let params = {
+			apiOrderSubmitForm: {
+				"client": 1,
+				"include": "items",
+				"shop_id": shopId,
+				"address_id": 0,
+				"efficiency": 0,
+				"efficiency_time": 0,
+				"shop_limit_time": 0,
+				"period_id": null,
+				"phone": "13202547840",
+				"remarks": "",
+				"is_takeaway": 0,
+				"box_fee": 0,
+				"total_fee": Number(price),
+				products,
+				"set_products": [],
+				"combos": [],
+				"shop_business_is": 0,
+				"user_rights": [],
+				"coupon_library_id": couponId ? couponId : "",
+				"orderTotalFee": Number(price),
+				"pmsData": {
+					"10041002": {
+						"clientScene": 6,
+						"isStudentMember": false,
+						"timestamp": +new Date(),
+						"isMemberPlus": false
+					},
+					"10041003": {
+						"benefitNos": ""
+					}
+				},
+				"packInfo": {
+					"eat_method": "TAKE-OUT",
+					"pack_method": "PAPER_BAG",
+					"box_fee": 0
+				}
+			}
+		}
+		console.log(JSON.stringify(params))
+		const { data: result } = await axios.post(`https://go.heytea.com/api/service-oms-order/grayapi/order/submit`, params, {
+			headers: {
+				'Authorization': latestToken.value,
+				'Content-Type': 'application/json'
+			}
+		})
+		if (![200, 0].includes(result.code)) {
+			res.send({ code: result.code, message: result.message })
+			return
+		}
+		const { order_no } = result.data
+		const { data: result1 } = await axios.post(`https://go.heytea.com/api/service-oms-order/grayapi/order/prepay`, {
+			order_no,
+			trade_type: "CENTER",
+			selectWallet: true,
+			hasWallet: false,
+			paymentSchema: ""
+		}, {
+			headers: {
+				'Authorization': latestToken.value,
+				'Content-Type': 'application/json'
+			}
+		})
+		if (![200, 0].includes(result1.code)) {
+			res.send({ code: result1.code, message: result1.message })
+			return
+		}
+		if (result1.data.length > 1) {
+			res.send({ code: 500, message: '余额不足，请联系卖家' })
+			return
+		}
+		// 将 order_no 和 signal 存入 MongoDB
+        const orderSignal = new OrderSignal({ order_no, signal })
+        await orderSignal.save()
 
-// // 更新订单
-// app.put('/order/:uuid', async (req, res) => {
-//     try {
-//         const order = await Order.findOneAndUpdate({ uuid: req.params.uuid }, req.body, { new: true, runValidators: true })
-//         if (!order) {
-//             return res.status(404).send()
-//         }
-//         res.send(order)
-//     } catch (error) {
-// 		console.log(error)
-//         res.status(400).send(error)
-//     }
-// })
+		res.send({ code: 200, message: '下单成功', data: result1 })
+	} catch (err) {
+		console.log('-----------------', err)
+		res.send({ code: err.status, message: err.data?.message || '请求失败' })
+	}
+})
 
-// // 删除订单
-// app.delete('/order/:uuid', async (req, res) => {
-//     try {
-//         const order = await Order.findOneAndDelete(req.params.uuid)
-//         if (!order) {
-//             return res.status(404).send()
-//         }
-//         res.send(order)
-//     } catch (error) {
-//         res.status(500).send(error)
-//     }
-// })
+// 检查订单号是否存在
+app.get('/checkOrder', async (req, res) => {
+    const { signal } = req.query
+    if (!signal) {
+        return res.status(400).send({ code: 400, message: '请提供 signal 参数' })
+    }
+    try {
+        const orderSignal = await OrderSignal.findOne({ signal })
+        if (orderSignal) {
+            res.send({ code: 200, message: '订单号存在', data: true })
+        } else {
+            res.send({ code: 200, message: '订单号不存在', data: false })
+        }
+    } catch (err) {
+        res.status(500).send({ code: 500, message: '服务器错误', error: err.message })
+    }
+})
 
-// // 获取订单列表
-// app.get('/orderList', async (req, res) => {
-//     const { pageSize = 10, pageNo = 1, buyer } = req.query
-//     const limit = parseInt(pageSize)
-//     const skip = (parseInt(pageNo) - 1) * limit
-// 	const query = buyer ? { buyer: new RegExp(buyer, 'i') } : {}
+app.get('/orderDetail', async (req, res) => {
+    const { signal } = req.query
+    if (!signal) {
+        return res.status(400).send({ code: 400, message: '请提供 signal 参数' })
+    }
+    try {
+		const orderSignal = await OrderSignal.findOne({ signal })
+		const { order_no } = orderSignal
+		const latestToken = await Token.findOne().sort({ createdAt: -1 })
+		if (!latestToken) {
+			return res.status(400).send({ code: 400, message: '未找到有效的 token，请先设置 token' })
+		}
+        const { data: result } = await axios.get(`https://go.heytea.com/api/service-oms-order/grayapi/order/detail?orderNo=${order_no}`, {
+			headers: {
+				'Authorization': latestToken.value
+			}
+		})
+		res.send({ code: 200, message: '获取成功', data: result })
+    } catch (err) {
+        res.send({ code: err.status, message: err.data?.message || '请求失败' })
+    }
+})
 
-//     try {
-//         const orders = await Order.find(query).sort({ createTime: -1 }).limit(limit).skip(skip)
-//         const total = await Order.countDocuments(query)
-//         res.send({ total, data: orders })
-//     } catch (error) {
-//         res.status(500).send(error)
-//     }
-// })
-
-// // 有路搜索
-// app.get('/youlu', async (req, res) => {
-// 	const { isbn } = req.query
-// 	const { data: html } = await axios.get(`https://www.youlu.net/search/result3/?isbn=${isbn}`)
-// 	res.setHeader('Content-Type', 'text/html')
-// 	res.send(html)
-// })
-
-// // 有路详情
-// app.get('/youlu/detail', async (req, res) => {
-// 	const { bookId } = req.query
-// 	const { data: detail } = await axios.get(`https://www.youlu.net/info3/bookBuy.aspx?bookId=${bookId}`)
-// 	res.send(detail)
-// })
-
-// // 有路加购
-// app.get('/youlu/addCart', async (req, res) => {
-// 	const { bookId, buyCount, cookie } = req.query
-// 	const { data: result } = await axios.get(`https://www.youlu.net/info3/youluBuy.aspx?bookId=${bookId}&buyCount=${buyCount}&category=old`, {
-// 		headers: { cookie }
-// 	})
-// 	res.send(result)
-// })
-
-
-// // 小谷吖搜索
-// app.get('/xiaoguya', async (req, res) => {
-// 	const { isbn, token } = req.query
-// 	try { 
-// 		const { data: result } = await axios.get(`https://api.xiaoguya.com:9898/mall/api/mall/product/search/searchProduct?current=1&size=20&keyword=${isbn}`, {
-// 			headers: {
-// 				'Authorization': `bearer ${token}`,
-// 				'Content-Type': 'application/json'
-// 			}
-// 		})
-// 		res.send(result)
-// 	} catch(err) {
-// 		if (err.response.status === 401) {
-// 			res.send({ code: 401, message: '小谷Token已过期，请更新Token'})
-// 		}
-// 	}
-// })
-
-// // 小谷吖详情
-// app.get('/xiaoguya/detail', async (req, res) => {
-// 	const { bookId, token } = req.query
-// 	try {
-// 		const { data: result } = await axios.get(`https://api.xiaoguya.com:9898/mall/api/mall/product/infoById/${bookId}`, {
-// 			headers: {
-// 				'Authorization': `bearer ${token}`,
-// 				'Content-Type': 'application/json'
-// 			}
-// 		})
-// 		res.send(result)
-// 	} catch(err) {
-// 		if (err.response.status === 401) {
-// 			res.send({ code: 401, message: '小谷Token已过期，请更新Token'})
-// 		}
-// 	}
-// })
-
-// // 小谷吖加购
-// app.get('/xiaoguya/addCart', async (req, res) => {
-// 	const { specId, token, count, isbn } = req.query
-// 	try {
-// 		const { data: result } = await axios.post(`https://api.xiaoguya.com:9898/mall/api/mall/cart/add/${specId}/${count}`, {}, {
-// 			headers: {
-// 				'Authorization': `bearer ${token}`
-// 			}
-// 		})
-// 		res.send(result)
-// 	} catch(err) {
-// 		if (err.response.status === 401) {
-// 			res.send({ code: 401, message: '小谷Token已过期，请更新Token'})
-// 		}else if (err.response.data?.code === 19003) {
-// 			res.send({ code: 400, message: '该商品限购4个', data: { isbn } })
-// 		}
-// 	}
-// })
-
-// // 星辰搜索
-// app.get('/xc', async (req, res) => {
-// 	const { isbn } = req.query
-// 	const { data: result } = await axios.get(`https://book.xclink.cn/xc-app/linkitembook/searchList?pageNum=0&pageSize=10&condition=${isbn}&typeId=&typeId2=&isStock=0&isPriceSort=0`)
-// 	res.send(result)
-// })
-
-// // 星辰详情
-// app.get('/xc/detail', async (req, res) => {
-// 	const { bookId } = req.query
-// 	const { data: result } = await axios.get(`https://book.xclink.cn/xc-app/linkitembook/bookCondition?bookId=${bookId}`)
-// 	res.send(result)
-// })
-
-// // 星辰加购
-// app.get('/xc/addCart', async (req, res) => {
-// 	const { itemId, token, num, conditionId, specificationId } = req.query
-// 	const { data: result } = await axios.post(`https://book.xclink.cn/xc-app/linkshoppingcart/save`, {
-// 		itemId, token, num, conditionId, specificationId
-// 	}, {
-// 		headers: { "Content-Type": 'application/x-www-form-urlencoded' }
-// 	})
-// 	if (result.message === '请登录' && result.status === 2) {
-// 		res.send({ code: 401, message: '请设置星辰Token' })
-// 		return
-// 	}
-// 	res.send(result)
-// })
-
-// // 孔夫子搜索
-// app.get('/kfz', async (req, res) => {
-// 	const { isbn, cookie } = req.query
-// 	const { data: result } = await axios.get(`https://search.kongfz.com/pc-gw/search-web/client/pc/product/keyword/list?dataType=0&keyword=${isbn}&page=1&size=50&sortType=7&actionPath=sortType&userArea=12001000000`, {
-// 		headers: { cookie }
-// 	})
-// 	res.send(result)
-// })
-
-// // 孔夫子详情
-// app.get('/kfz/detail', async (req, res) => {
-// 	const { shopId, itemId, cookie } = req.query
-// 	const { data: html } = await axios.get(`https://book.kongfz.com/${shopId}/${itemId}/`, {
-// 		headers: { cookie }
-// 	})
-// 	res.setHeader('Content-Type', 'text/html')
-// 	res.send(html)
-// })
-
-// // 孔夫子加购
-// app.get('/kfz/addCart', async (req, res) => {
-// 	const { itemId, shopId, numbers, cookie } = req.query
-// 	const { data: result } = await axios.get(`https://cart.kongfz.com/jsonp/add?&itemId=${itemId}&shopId=${shopId}&numbers=${numbers}`, {
-// 		headers: { cookie }
-// 	})
-// 	if (numbers > 1) {
-// 		const cartId = result.result.cartId + ''
-// 		await axios.post(`https://cart.kongfz.com/cart-web/pc/v1/cart/updateCartNum`, {}, {
-// 			params: { cartId, number: numbers },
-// 			headers: { cookie }
-// 		})
-// 	}
-// 	res.send(result)
-// })
-
-// //孔夫子获取cookie
-// app.get('/kfz/getCookie', async (req, res) => {
-// 	const result = await axios.post(`https://login.kongfz.com/Pc/Login/account`, {
-// 		loginName: '13202547840',
-// 		loginPass: 'chenweiqq0'
-// 	}, {
-// 		headers: { "Content-Type": 'application/x-www-form-urlencoded' }
-// 	})
-// 	const cookie = result.headers['set-cookie'].find(f => f.startsWith('PHPSESSID')).split(';')[0]
-// 	res.send(cookie)
-// })
-
-// // 旧书云搜索
-// app.get('/jsy', async (req, res) => {
-// 	const { isbn } = req.query
-// 	const { data: result } = await axios.get(`https://www.jiushuyunshop.com/api/collect/shop/list?category_id=1&keywords=${isbn}&limit=10&page=1&order=price asc`)
-// 	res.send(result)
-// })
-
-// // 旧书云加购
-// app.get('/jsy/addCart', async (req, res) => {
-// 	const { bookId, quantity, token } = req.query
-// 	const { data: result } = await axios.post(`https://www.jiushuyunshop.com/api/collect/shop/car/add`, 
-// 		{
-// 			goods_id: bookId,
-// 			goods_num: quantity
-// 		},
-// 		{
-// 			headers: { 'Authorization': token }
-// 		}
-// 	)
-// 	if (result.code === 400) {
-// 		res.send({ code: 401, message: '请设置旧书云Token' })
-// 		return
-// 	}
-// 	res.send(result)
-// })
+// 查找优惠券
+app.post('/findCoupon', async (req, res) => {
+	try {
+		const latestToken = await Token.findOne().sort({ createdAt: -1 })
+		if (!latestToken) {
+			return res.status(400).send({ code: 400, message: '未找到有效的 token，请先设置 token' })
+		}
+		const { data: result } = await axios.post(`https://vip.heytea.com/api/service-coupon/couponLibrary/unused-page/v2`, req.body, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': latestToken.value
+			}
+		})
+		res.send({ code: 200, message: '获取成功', data: result.data })
+	} catch (err) {
+		res.send({ code: err.status, message: err.data?.message || '请求失败' })
+	}
+})
 
 app.listen(3000, () => {
 	console.log("启动成功！")
